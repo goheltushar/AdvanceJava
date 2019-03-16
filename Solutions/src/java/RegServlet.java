14c4cb0de402e1c9dace21e4c1ead600f7fd0903
administrator <administrator@tg-ubuntu>
2019-03-16 09:53:44 +0530

    Solution to Tutorial 3 of Advance Java

---
+++ Solutions/src/java/RegServlet.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class RegServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+
+        String id = req.getParameter("empid");
+        String name = req.getParameter("empname");
+        String email = req.getParameter("empemail");
+        int age = Integer.parseInt(req.getParameter("empage"));
+        
+        out.println(id + "<br>" + name + "<br>" + email + "<br>" + age);
+    }
+}
---
+++ Solutions/src/java/RegistraionServlet.java
@@ -0,0 +1,31 @@
+
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(name = "RegistraionServlet", urlPatterns = "/RegistraionServlet")
+public class RegistraionServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+
+        String id = req.getParameter("empid");
+        String name = req.getParameter("empname");
+        String email = req.getParameter("empemail");
+        int age = Integer.parseInt(req.getParameter("empage"));
+
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection con = DriverManager.getConnection("jdbc:oracle:thin:@//172.16.17.3:1521/orcl1", "gtushar", "staff");
+            Statement smt = con.createStatement();
+            smt.execute("insert into emp values('" + id + "', '" + name + "', '" + email + "'," + age + "");
+            out.print("Record Inserted Succesfully...");
+        } catch (Exception e) {
+            out.print(e);
+        }
+
+    }
+}
